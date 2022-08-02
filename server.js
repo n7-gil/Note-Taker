561243110b3c5f7998fdb342014ff7076222365b
Gilbert Escobedo <gib@Gilberts-MBP.lan>
2022-08-01 20:09:03 -0700

    started writing some GET commands on server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+
 app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => res.json(noteData));
+
+app.get('/api/index/*', (req, res)) ; res.json();
