72d129aae846e28ad34b2ada7c9c8ca625dc6543
N7 GIL <gilbertoaescobedo@gmail.com>
2022-08-02 10:46:09 -0700

    fixed issues with not linking css and js properly

---
+++ Develop/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.use(express.static(__dirname + '/public'));
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/public/index.html'));
+// });
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.listen(PORT, () => {
+console.log(`App listening to ${PORT}`)
+})
+
+
